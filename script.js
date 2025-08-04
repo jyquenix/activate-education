1ade11779107581c29cbf30ad06d55bf5ce2ab2d
jyquenix <jyquenixreal@gmail.com>
2025-08-03 22:21:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -139,9 +139,3 @@ $('#cForm')?.addEventListener('submit', e => {
 
 
 
-/* scripts.js */
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    alert('Purchase functionality not implemented.');
-  });
-});
